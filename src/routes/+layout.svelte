21f1ff8b825f12a78b252ca85f172f2c46e0d782
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-22 18:19:13 +0900

    레이아웃 파일에 css 추가

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<!-- layout 파일은 각 링크된 page의 dom정보를 -->
+<script lang="ts">
+	// css 로딩시 이렇게
+	// layout에 넣어야 slot에 바뀌는 값에도 각각 적용이 가능하기 떄문에?
+	import '../app.css'
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/posts">Posts</a>
+		</li>
+	</ul>
+</nav>
+
+<!-- 슬롯이 위치한곳에 뿌려준다. -->
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,18 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte'
 
+	// 마운트 되었을때
+	// onMount는 본 컴포넌트가 처음 DOM에 렌더링 될 때 실행되는 함수
+	// api 같이 데이터를 한번만 가져와야하는 경우에 유용한 함수
 	onMount(() => {
 		console.log('Mounted home page')
 	})
+
+	// 마운트 되지 않았을때
+	// onDestroy는 본 컴포넌트가 DOM에서 제거되었을때 실행되는 함수
 	onDestroy(() => {
 		console.log('Unmounted home page')
 	})
 </script>
-
-<nav>
-	<ul>
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/posts">Posts</a>
-		</li>
-	</ul>
-</nav>
 
 <h1>Home Page</h1>
