76fc2d3292bd7e761a3d1be5182e37896a964d8b
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-22 17:59:54 +0900

    first upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte'
+
+	onMount(() => {
+		console.log('Mounted home page')
+	})
+	onDestroy(() => {
+		console.log('Unmounted home page')
+	})
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/posts">Posts</a>
+		</li>
+	</ul>
+</nav>
+
+<h1>Home Page</h1>
